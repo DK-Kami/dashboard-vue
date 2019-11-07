f4a46785fc481768ba09e1bf8854e4952234e152
Kami San <1.dankon.1@gmail.com>
2019-11-08 02:30:21 +0300

    Display connected accounts

--- src/views/Profile/AccountsUserData.vue
+++ src/views/Profile/AccountsUserData.vue
@@ -1,5 +1,62 @@
 <template>
-  <v-card class="pa-8">
-    AccountsUserData
+  <v-card class="pa-8 py-4">
+    <v-card-title class="text-center">
+      Подключённые аккаунты
+      <v-spacer />
+
+      <tooltip-button
+        tooltip="Редактировать"
+        icon="edit"
+        lighten="1"
+        top
+        @action="editProfile"
+      />
+    </v-card-title>
+
+    <v-card-text>
+      <v-list>
+        <v-list-item v-for="item in contacts" :key="item._id">
+          <v-list-item-content>{{ item.name }}</v-list-item-content>
+
+          <v-list-item-content class="align-end">
+            <v-list-item-title class="text-right">
+              {{ item.value }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
   </v-card>
 </template>
+
+<script>
+import TooltipButton from '@/components/TooltipButton';
+
+export default {
+  name: 'AccountsUserData',
+  components: { TooltipButton },
+  data: () => ({
+    isEdit: false,
+  }),
+  computed: {
+    contactsTypes() {
+      return this.$store.getters['types/getContactTypes'];
+    },
+    contacts() {
+      return this.$store.getters['user/getUserContacts'];
+    },
+  },
+  methods:{
+    async loadContactTypes() {
+      this.$store.dispatch('types/loadContactTypes');
+    },
+
+    editProfile() {
+      if (!this.contactsTypes.length) {
+        this.loadContactTypes();
+      }
+      this.isEdit = true;
+    },
+  },
+};
+</script>
--- src/views/Profile/MainUserData.vue
+++ src/views/Profile/MainUserData.vue
@@ -1,25 +1,27 @@
 <template>
   <v-card>
     <v-card-title>
-      <v-layout justify-center wrap>
+      <v-layout align-center column>
         <v-avatar
           class="white--text mt-n12 elevation-10"
           color="accent"
           size="80"
         >{{ user.avatar }}</v-avatar>
 
-        <v-flex xs12 class="text-right mt-n11">
-          <tooltip-button
-            tooltip="Редактировать"
-            icon="edit"
-            lighten="1"
-            top
-            @action="editProfile"
-          />
-        </v-flex>
+        <v-tooltip top>
+          <template #activator="{ on }">
+            <div
+              class="headline mt-4 ml-6 cursor--pointer"
+              @click="editProfile"
+              v-on="on"
+            >
+              {{ user.nickname }}
+              <v-icon class="ml-1 grey--text text--lighten-1">edit</v-icon>
+            </div>
+          </template>
+          Редактировать
+        </v-tooltip>
 
-        <div class="headline mt-4">{{ user.nickname }}</div>
-        <v-flex xs12 />
         <div class="subtitle-2 grey--text">{{ user.email }}</div>
       </v-layout>
     </v-card-title>
@@ -28,13 +30,13 @@
       <v-card-text class="px-12">
         <v-list class="px-8">
           <v-list-item v-for="item in info" :key="item._id">
-            <v-list-item-title>
-              {{ item.name }}
-            </v-list-item-title>
+            <v-list-item-content>{{ item.name }}</v-list-item-content>
 
-            <v-list-item-action>
-              {{ item.value }}
-            </v-list-item-action>
+            <v-list-item-content class="align-end">
+              <v-list-item-title class="text-right">
+                {{ item.value }}
+              </v-list-item-title>
+            </v-list-item-content>
           </v-list-item>
         </v-list>
       </v-card-text>
