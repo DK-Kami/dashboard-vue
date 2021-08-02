eafffd50caab04e3ed3c987125eec6dea081c235
Kami-san <1.dankon.1@gmail.com>
2021-08-02 23:11:45 +0300

    add aliases for path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  chainWebpack: config => {
+    config.resolve.alias
+    .set('@services', resolve('src/middleware/services'))
+    .set('@mdw', resolve('src/middleware'))
+    .set('@cmp', resolve('src/components'))
+    .set('@mixin', resolve('/src/helper/mixins'))
+  }
 }
