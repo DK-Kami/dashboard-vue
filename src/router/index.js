42a38df6cbf30dd43997a35b25ae0173f9cd4f34
Kami-san <1.dankon.1@gmail.com>
2021-08-11 22:38:29 +0300

    create router

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,56 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import initListners from './initListners';
 import listners from './listners';
+import LoginPage from '@/views/LoginPage';
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    name: 'login',
+    path: '/login',
+    meta: {
+      access: true,
+    },
+    component: LoginPage,
+  },
+  {
+    name: 'registration',
+    path: '/registration',
+    meta: {
+      access: true,
+    },
+    component: LoginPage,
+  },
+
+  {
+    name: 'profile',
+    path: '/profile',
+    component: () => import('@/views/Profile'),
+  },
+  {
+    name: 'dashboard',
+    path: '/dashboard',
+    component: () => import('@/views/Dashboard'),
+  },
+  {
+    name: 'settings',
+    path: '/settings',
+    component: () => import('@/views/Settings'),
+  },
+  {
+    name: 'AnonimDashboard',
+    path: '/fs/:token/:nickname',
+    meta: {
+      access: true,
+    },
+    component: () => import('@/views/AnonimDashboard'),
+  },
+  {
+    path: '/*',
+    redirect: '/dashboard',
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
