a0f0d65f0e3854f9331a6a66f8ab3b290e22c2e6
Kami-san <1.dankon.1@gmail.com>
2021-08-14 22:40:03 +0300

    set themes colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,12 +11,12 @@ export default new Vuetify({
       },
     themes: {
       light: {
-        primary: '#007BFF',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
+        primary: '#070924',
+        secondary: '#44476D',
+        accent: '#D500F9',
+        error: '#D32F2F',
+        info: '#00ACC1',
+        success: '#00E676',
         warning: '#FFC107'
       },
     },
