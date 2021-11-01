a48252c0db5afd3b36aef3b3adacec4a06f801fa
Kami-san <1.dankon.1@gmail.com>
2021-11-01 22:08:20 +0300

    login form

--- src/views/LoginPage/index.vue
+++ src/views/LoginPage/index.vue
@@ -3,11 +3,43 @@
     <el-row class="login__card">
       <el-col :span="8" class="login__selector">
         <div class="selector">
-          <div class="selector__item active__item">Login <div class="triangle" /></div>
-          <div class="selector__item active__item">Register <div class="triangle" /></div>
+          <div
+            :class="{ 'active__item' : isAuth }"
+            class="selector__item"
+            @click="isAuth = true"
+          >Login <div class="triangle" /></div>
+
+          <div
+            :class="{ 'active__item' : isReg }"
+            class="selector__item"
+            @click="isAuth = false"
+          >Register <div class="triangle" /></div>
         </div>
       </el-col>
-      <el-col :span="16">2</el-col>
+
+      <el-row class="login-form">
+        <el-col class="login__tip">forgot password?</el-col>
+        <el-col class="form-container">
+          <el-form
+            v-if="isAuth"
+            :model="authData"
+            label-position="top"
+            ref="authForm"
+          >
+            <el-form-item label="Login" prop="login" required>
+              <el-input v-model="authData.login" />
+            </el-form-item>
+
+            <el-form-item label="Password" prop="password" required>
+              <el-input v-model="authData.password" type="password" />
+            </el-form-item>
+
+            <el-form-item >
+              <el-button>Login</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
     </el-row>
   </div>
 </template>
@@ -15,6 +47,21 @@
 <script>
 export default {
   name: 'LoginPage',
+
+  data: () => ({
+    isAuth: true,
+
+    authData: {
+      login: '',
+      password: '',
+    },
+  }),
+
+  computed: {
+    isReg(){
+      return !this.isAuth;
+    },
+  },
 };
 </script>
 
@@ -55,24 +102,59 @@ export default {
 
     .selector__item {
       display: flex;
-      color: #fff;
+      justify-content: space-between;
       font-size: 40px;
       padding: 20px 0;
-      width: 200px;
+      width: 150px;
+      height: 50px;
+      cursor: pointer;
+      color: #ffffff77;
     }
   }
 
-  .active__item>.triangle::before {
-    content: "";
-    width: 0;
-    height: 0;
-    border-style: solid;
-    border-width: 30px 52.0px 30px 0;
-    border-color: transparent white transparent transparent;
-    display: inline-block;
-    position: relative;
-    left: 70px;
-    top: -5px;
+  .active__item {
+    color: #fff !important;
+
+    .triangle {
+      width: 0px;
+    }
+
+    .triangle::before {
+      content: "";
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 30px 52.0px 30px 0;
+      border-color: transparent white transparent transparent;
+      display: inline-block;
+      position: relative;
+      left: 15px;
+      top: -5px;
+    }
+  }
+}
+
+.login-form {
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  // align-self: flex-start;
+  align-self: stretch;
+
+  .login__tip {
+    text-align: end;
+    font-size: 12px;
+    padding: 5px;
+    color: #00000077;
+    cursor: pointer;
+    transition: .4s;
+  }
+  .login__tip:hover {
+    color: #000000aa;
+  }
+
+  .form-container {
+    max-width: 350px;
   }
 }
 </style>
